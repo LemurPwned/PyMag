new data holder fix + resistance formula fix

pymag/engine/backend.py
@@ -29,15 +29,13 @@ def compute_vsd(stime, m_xs, frequency, integration_step):
 def calculate_resistance(Rx0, Ry0, AMR, AHE, SMR, m, number_of_layers, l, w):
     R_P = Rx0[0]
     R_AP = Ry0[0]
-
     SxAll = np.zeros((number_of_layers, ))
     SyAll = np.zeros((number_of_layers, ))
     for i in range(0, number_of_layers):
         w_l = w[i] / l[i]
-        SxAll[i] = 1 / (Rx0[i] + Rx0[i] * AMR[i] * m[i, 0]**2 +
-                        Rx0[i] * SMR[i] * m[i, 1]**2)
-        SyAll[i] = 1 / (Ry0[i] + AHE[i] * m[i, 2] + Rx0[i] * (w_l) *
-                        (AMR[i] + SMR[i]) * m[i, 0] * m[i, 1])
+        SxAll[i] = 1 / (Rx0[i] + (AMR[i] * m[i, 0]**2 + SMR[i] * m[i, 1]**2))
+        SyAll[i] = 1 / (Ry0[i] + 0.5 * AHE[i] * m[i, 2] + (w_l) *
+                        (SMR[i] - AMR[i]) * m[i, 0] * m[i, 1])
 
     Rx = 1 / np.sum(SxAll)
     Ry = 1 / np.sum(SyAll)
@@ -46,7 +44,6 @@ def calculate_resistance(Rx0, Ry0, AMR, AHE, SMR, m, number_of_layers, l, w):
         Rz = R_P + (R_AP - R_P) / 2 * (1 - np.sum(m[0, :] * m[1, :]))
     else:
         Rz = 0
-
     return Rx, Ry, Rz
 
 
@@ -213,4 +210,4 @@ class SolverTask(QtCore.QThread):
                 self.progress.emit(progr)
                 final_PIMM.append(partial_result.PIMM.tolist()[0])
             self.queue.put((sim_index, ..., SimulationStatus.DONE))
-        self.queue.put(({},..., SimulationStatus.ALL_DONE))
+        self.queue.put(({}, ..., SimulationStatus.ALL_DONE))

pymag/engine/data_holders.py
@@ -1,9 +1,11 @@
 import json
 import os
 from re import M
+import re
 
 from pymag.engine.utils import get_stimulus
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
+from pydantic import BaseModel
 import numpy as np
 import pandas as pd
 
@@ -49,18 +51,51 @@ class GenericHolder:
         cls(**dict_)
 
 
-class ExperimentData(GenericHolder):
-    def __init__(self, name: str, H: Union[List[float], np.ndarray],
-                 f: Union[List[float], np.ndarray]) -> None:
-        self.H = H
-        self.f = f
-        self.name = name
+class ExperimentData(GenericHolder, BaseModel):
+    name: str
+    H: List[float] = []
+    theta: List[float] = []
+    phi: List[float] = []
+    f: List[float] = []
+    Vmix: List[float] = []
+    Rx: List[float] = []
+    Ry: List[float] = []
+    Rz: List[float] = []
+    Mx: List[float] = []
+    My: List[float] = []
+    Mz: List[float] = []
 
-    @classmethod
-    def from_csv(cls, filename):
+    x: List[float] = []
+
+    @staticmethod
+    def from_csv(filename):
         bsn = os.path.basename(filename)
-        df = pd.read_csv(filename, sep='\t')
-        return cls(name=bsn, H=df['H'], f=df['f'])
+        df: pd.DataFrame = pd.read_csv(filename, sep='\t').astype(float)
+        t = df.to_dict(orient='list')
+        expr = ExperimentData(name=bsn, **t)
+        expr.deduce_change()
+        return expr
+
+    def deduce_change(self):
+        if len(np.unique(self.H)) == 1:
+            if len(np.unique(self.phi)) != 1:
+                self.x = self.phi
+            else:
+                self.x = self.theta
+        else:
+            self.x = self.H
+
+    def get_vsd_series(self):
+        return self.x, self.Vmix
+
+    def get_m_series(self):
+        return self.x, self.Mx, self.My, self.Mz
+
+    def get_r_series(self):
+        return self.x, self.Rx, self.Ry, self.Rz
+
+    def get_pimm_series(self):
+        return self.x, self.f
 
 
 class ResultHolder(GenericHolder):

pymag/gui/plot_manager.py
@@ -83,6 +83,8 @@ class PlotManager:
     def clear_experiment_plots(self):
         self.SD_plot.plot_image.clearPlots()
         self.PIMM_plot.plot_image.clearPlots()
+        self.resistance_plot.clear_experimental()
+        self.magnetisation_plot.clear_experimental()
 
     def plot_active_results(self, obj_list: List[Union[Simulation,
                                                        ExperimentData]]):
@@ -107,9 +109,25 @@ class PlotManager:
             self.plot_simulation(plot_input.get_simulation_result())
 
     def plot_experiment(self, data: ExperimentData):
-        # sort
-        H, f = zip(*sorted(zip(data.H, data.f)))
-        self.SD_plot.update_plot(np.asarray(H), np.asarray(f))
+        """
+        Plot the experimental data if it exists
+        """
+        x, f = data.get_pimm_series()
+        if f:
+            self.PIMM_plot.update_plot(np.asarray(x), np.asarray(f))
+        x, vmix = data.get_vsd_series()
+        if vmix:
+            self.SD_plot.update_plot(np.asarray(x), np.asarray(vmix))
+
+        x, Rx, Ry, Rz = data.get_r_series()
+        for i, R in enumerate((Rx, Ry, Rz)):
+            if R:
+                self.resistance_plot.set_experimental(i, x, R)
+
+        x, Mx, My, Mz = data.get_r_series()
+        for i, M in enumerate((Mx, My, Mz)):
+            if M:
+                self.magnetisation_plot.set_experimental(i, x, M)
 
     def plot_simulation(self, result_holder: ResultHolder):
         """

pymag/gui/plots.py
@@ -1,6 +1,8 @@
+from typing import List
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
 import numpy as np
+from pyqtgraph.graphicsItems.PlotDataItem import PlotDataItem
 
 
 class MultiplePlot():
@@ -10,7 +12,8 @@ class MultiplePlot():
         self.plot_area.setGeometry(QtCore.QRect(0, 0, 600, 300))
         self.plot_area.nextRow()
         self.plot_area.setBackground('w')
-        self.plots = []
+        self.plots: List[pg.PlotItem] = []
+        self.experimental_plots: List[pg.PlotDataItem] = []
 
         for i in range(0, number_of_plots):
             self.plots.append(self.plot_area.addPlot())
@@ -33,7 +36,20 @@ class MultiplePlot():
         self.plots[i].setLabel('bottom', bottom_caption, units=bottom_units)
 
     def set_plot(self, n, X, Y):
-        self.plots[n].plot(np.array(X), np.array(Y))
+        self.plots[n].plot(np.asarray(X), np.asarray(Y))
+
+    def set_experimental(self, n, X, Y):
+        pointer = self.plots[n].scatterPlot(np.asarray(X),
+                                            np.asarray(Y),
+                                            pen=pg.mkPen('r', width=1.2),
+                                            alpha=0.6)
+        self.experimental_plots.append(pointer)
+
+    def clear_experimental(self):
+        exp_plot: PlotDataItem
+        for i, exp_plot in enumerate(self.experimental_plots):
+            self.plots[i].removeItem(exp_plot)
+        self.experimental_plots.clear()
 
 
 class SpectrogramPlot():